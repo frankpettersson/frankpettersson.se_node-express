9887b1cf2f3166b3a5a927381c8c8f890a667b04
Frank Pettersson <frank.pettersson1@gmail.com>
2019-02-13 16:15:35 +0100

    idle_0.2

--- games/idle/js/htmlgen.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-function baseHTML() {
-    $('.wrapwrap').append($('<div>')
-        .addClass('wrapper')
-    );
-}
--- games/idle/js/main.js
+++ games/idle/js/main.js
@@ -1,152 +1,135 @@
-//Resource--------------------------------------------------------------------------------------
+//Resource_________________________________________________________________________________________
 let things = {
-    currentNumber: 0,
-    totalTick: 0,
-    addNumber: 1,
-    clickUpCost: 10,
-    manualClickTotal: 0,
+    value: {
+        currentNumber: 0,
+        totalTick: 0,
+        addNumber: 1,
+        clickUpCost: 10,
+        manualClickTotal: 0
+    },
+    ids: {
+        currentNumberID: 'currentNumber',
+        totalTickID: 'totalTick',
+        addNumberID: 'addNumber',
+        clickUpCostID: 'clickUpCost',
+        manualClickTotalID: 'manualClickTotal',
+        unitbox: 'unitbox',
+        basetickID: 'basetick',
+        costID: 'cost',
+        tickID: 'tick',
+        ownedID: 'owned',
+        earnedID: 'earned',
+        nameID: 'name',
+        buyID: 'buy'
+    },
+    class: {
+        wrapwrapwrap: 'wrapwrapwrap',
+        wrapwrap: 'wrapwrap',
+        wrapper: 'wrapper',
+        box: 'box',
+        outerbox: 'outerbox',
+        innerbox: 'innerbox',
+        button: 'button',
+        upgradebox: 'upgradebox',
+        upgrade: 'upgrade'
+    },
     units: [{
-        owned: 0,
-        basetick: 1.0,
-        tick: 0,
         basecost: 50.0,
+        basetick: 1.0,
         cost: 50.0,
+        owned: 0,
+        tick: 0,
         earned: 0,
         singularName: 'clickman',
-        pluralName: 'clickmen',
-        nameClass: '.unit1Name',
-        nameID: '#unit1Name',
-        basetickID: '#unit1Basetick',
-        ownedID: '#unit1Owned',
-        tickID: '#unit1Tick',
-        costID: '#unit1Cost',
-        earnedID: '#unit1Earned',
-        buyID: 'unit1Buy'
+        pluralName: 'clickmen'
     }, 
     {
-        owned: 0,
-        basetick: 2.5,
-        tick: 0,
         basecost: 150.0,
+        basetick: 2.5,
         cost: 150.0,
+        tick: 0,
+        owned: 0,
         earned: 0,
         singularName: 'clickmonster',
-        pluralName: 'clickmonsters',
-        nameClass: '.unit2Name',
-        nameID: '#unit2Name',
-        ownedID: '#unit2Owned',
-        basetickID: '#unit2Basetick',
-        tickID: '#unit2Tick',
-        costID: '#unit2Cost',
-        earnedID: '#unit2Earned',
-        buyID: 'unit2Buy'
+        pluralName: 'clickmonsters'
     }, 
     {
-        owned: 0,
-        basetick: 5.0,
-        tick: 0,
         basecost: 300.0,
+        basetick: 5.0,
         cost: 300.0,
+        tick: 0,
+        owned: 0,
         earned: 0,
         singularName: 'clickalien',
-        pluralName: 'clickaliens',
-        nameClass: '.unit3Name',
-        nameID: '#unit3Name',
-        ownedID: '#unit3Owned',
-        basetickID: '#unit3Basetick',
-        tickID: '#unit3Tick',
-        costID: '#unit3Cost',
-        earnedID: '#unit3Earned'
-    }, 
+        pluralName: 'clickaliens'
+    },
     {
-        owned: 0,
-        basetick: 10.0,
-        tick: 0,
         basecost: 600.0,
+        basetick: 10.0,
         cost: 600.0,
+        tick: 0,
+        owned: 0,
         earned: 0,
         singularName: 'clickgod',
-        pluralName: 'clickgods',
-        nameClass: '.unit4Name',
-        nameID: '#unit4Name',
-        ownedID: '#unit4Owned',
-        basetickID: '#unit4Basetick',
-        tickID: '#unit4Tick',
-        costID: '#unit4Cost',
-        earnedID: '#unit4Earned'
+        pluralName: 'clickgods'
     }]
 };
-//--------------------------------------------------------------------------------------
+//_________________________________________________________________________________________________
 
-//Values--------------------------------------------------------------------------------------
+//Values___________________________________________________________________________________________
 let t = things;
+let uLen = t.units.length;
+let value = t.value;
+let ids = t.ids;
+let classes = t.class; 
 let u = t.units;
-let cn = t.currentNumber;
-let tt = t.totalTick;
-let cu = t.clickUpCost;
-let an = t.addNumber;
-let mc = t.manualClickTotal;
-//--------------------------------------------------------------------------------------
+let cn = value.currentNumber;
+let tt = value.totalTick;
+let cu = value.clickUpCost;
+let an = value.addNumber;
+let mc = value.manualClickTotal;
+//_________________________________________________________________________________________________
 
-//Set Values Once--------------------------------------------------------------------------------------
-$.each(things.units, function(i) {
-    $(u[i].nameID).html(u[i].pluralName);
-    u[i].tick = u[i].basetick * u[i].owned;
-});
-$('#addNumber').html(an + ' number');
-//--------------------------------------------------------------------------------------
-
-//Refresh Values-------------------------------------------------------------------------------
+//Refresh Values___________________________________________________________________________________
 function refreshValues() {
-    $.each(things.units, function(i) {
-        $.each(things.units[i], function() {
-            let bt = u[i].basetick;
+    $.each(u, function(i) {
+        $.each(u[i], function() {
             let ow = u[i].owned;
             let tc = u[i].tick;
             let co = u[i].cost;
             let ea = u[i].earned;
-            let si = u[i].singularName;
-            let pl = u[i].pluralName;
-            let nc = u[i].nameClass;
-            let bi = u[i].basetickID;
-            let oi = u[i].ownedID;
-            let ti = u[i].tickID;
-            let ci = u[i].costID;
-            let ei = u[i].earnedID;
-            $(oi).html(ow);
-            $(bi).html(bt);
-            $(ti).html(tc);
-            $(ci).html(co);
-            $(ei).html(ea);
-            if (ow === 1) {
-                $(nc).html(' '+si);
-            } else {
-                $(nc).html(' '+pl);
-            }
+            let oi = ids.ownedID+i;
+            let ti = ids.tickID+i;
+            let ci = ids.costID+i;
+            let ei = ids.earnedID+i;
+            $('#'+oi).html(ow);
+
+            $('#'+ti).html(tc);
+            $('#'+ci).html(co);
+            $('#'+ei).html(ea);
         });
     });
-    $('#currentNumber').html(cn);
-    $('#totalTick').html(tt);
-    $('#clickUpCost').html(cu);
-    $('#manualClickTotal').html(mc);
+    $('#'+ids.currentNumberID).html(cn);
+    $('#'+ids.totalTickID).html(tt);
+    $('#'+ids.clickUpCostID).html(cu);
+    $('#'+ids.manualClickTotalID).html(mc);
+    $('#'+ids.addNumberID).html(an);
 }
-//--------------------------------------------------------------------------------------
+//_________________________________________________________________________________________________
 
-//Click to make number go up related--------------------------------------------------------------------------------------
-// eslint-disable-next-line no-unused-vars
+//Click +__________________________________________________________________________________________
 function addClick() {
     cn += an;
     mc += an;
-    $('#currentNumber').html(cn);
-    $('#manualClickTotal').html(mc);
+    $('#'+ids.currentNumberID).html(cn);
+    $('#'+ids.manualClickTotalID).html(mc);
 }
-//--------------------------------------------------------------------------------------
+//_________________________________________________________________________________________________
 
-//Buy Unit--------------------------------------------------------------------------------------
-// eslint-disable-next-line no-unused-vars
+//Buy Unit_________________________________________________________________________________________
 function unitBuy(id) {
-    $.each(things.units, function(i) {
-        if (u[i].buyID === id) {
+    $.each(u, function(i) {
+        if (ids.buyID+i === id) {
             if(cn >= u[i].cost) {
                 u[i].owned++;
                 cn = cn - u[i].cost;
@@ -158,14 +141,194 @@ function unitBuy(id) {
         }
     });
 }
-//--------------------------------------------------------------------------------------
+//_________________________________________________________________________________________________
 
-//Idle gain related--------------------------------------------------------------------------------------
+//Idle gain related________________________________________________________________________________
 window.setInterval(function(){
     cn = cn + tt;
-    $.each(things.units, function(i) {
+    $.each(u, function(i) {
         u[i].earned += u[i].tick;
     });
     refreshValues();
 }, 1000);
-//--------------------------------------------------------------------------------------
+//_________________________________________________________________________________________________
+
+//Create HTML Body_________________________________________________________________________________
+$('body').append($('<div>', {
+    class: classes.wrapwrapwrap
+})
+    .append($('<div>', {
+        class: classes.wrapwrap
+    })
+        .append($('<div>', {
+            class: classes.wrapper
+        })
+            .append($('<div>', {
+                class: classes.box
+            })
+                .append($('<h1>')
+                    .text('numbers: ')
+                    .append($('<span>', {
+                        id: ids.currentNumberID
+                    }))
+                )
+                .append($('<p>')
+                    .append($('<b>')
+                        .append($('<span>', {
+                            id: ids.totalTickID
+                        }))
+                    )
+                    .append(' n/s')
+                )
+            )
+            .append($('<div>', {
+                class: classes.box
+            })
+                .append($('<h3>')
+                    .text('give me number!')
+                )
+                .append($('<div>', {
+                    class: classes.innerbox
+                })
+                    .append($('<p>')
+                        .append('click it!')
+                        .append($('<p>')
+                            .append($('<b>')
+                                .append($('<span>', {
+                                    id: ids.manualClickTotalID
+                                }))
+                            )
+                            .append(' numbers by clicks')
+                        )
+                    )
+                    .append($('<div>', {
+                        class: classes.button,
+                        onclick: 'addClick()'
+                    })
+                        .append($('<p>')
+                            .append('+')
+                            .append($('<span>', {
+                                id: ids.addNumberID
+                            }))
+                        )
+                    )
+                )
+            )
+            .append($('<div>', {
+                id: ids.unitbox
+            })
+                .append($('<h4>')
+                    .append('hire clickers!')
+                )
+                .append($('<h5>')
+                    .append($('<img>', {
+                        src: 'images/next.svg',
+                        style: 'width: 13px; height: 13px;',
+                        id: 'arrow1',
+                        onclick: `rotate(this.id, '.${classes.outerbox}')`
+                    }))
+                    .append(' clickos')
+                )
+                .append($('<div>', {
+                    class: classes.outerbox
+                })
+                )
+            )
+        )
+        .append($('<div>', {
+            class: classes.wrapper
+        })
+            .append($('<div>', {
+                class: classes.box
+            })
+            )
+        )
+    )
+);
+//_________________________________________________________________________________________________
+
+//Create Unit Elements_____________________________________________________________________________
+for(let i = 0; i < uLen; i++) {
+    $('.'+classes.outerbox).append($('<div>', {
+        class: classes.innerbox
+    })
+        .append($('<p>')
+            .append($('<b>')
+                .append($('<span>', {
+                    id: ids.nameID+i
+                }))
+            )
+            .append(', ')
+            .append($('<span>', {
+                id: ids.basetickID+i
+            }))
+            .append('n/s')
+        )
+        .append($('<p>')
+            .append('cost:')
+            .append($('<b>')
+                .append($('<span>', {
+                    id: ids.costID+i
+                }))
+            )
+            .append(' numbers')
+        )
+        .append($('<div>', {
+            class: classes.button,
+            id: ids.buyID+i,
+            onclick: 'unitBuy(this.id)'
+        })
+            .append('hire!')
+        )
+        .append($('<p>')
+            .append($('<img>', {
+                src: 'images/next.svg',
+                style: 'width: 13px; height: 13px;',
+                id: 'arrow'+(i+2),
+                onclick: `rotate(this.id, '#stats${i}')`
+            })
+                .append(' stats')
+            )
+        )
+        .append($('<div>', {
+            id: 'stats'+i
+        })
+            .append($('<p>')
+                .append('n/s:')
+                .append($('<b>')
+                    .append($('<span>', {
+                        id: ids.tickID+i
+                    }))
+                )
+            )
+            .append($('<p>')
+                .append('owned:')
+                .append($('<b>')
+                    .append($('<span>', {
+                        id: ids.ownedID+i
+                    }))
+                )
+                
+            )
+            .append($('<p>')
+                .append('earned:')
+                .append($('<b>')
+                    .append($('<span>', {
+                        id: ids.earnedID+i
+                    }))
+                )
+            )
+        )
+    );
+}
+//_________________________________________________________________________________________________
+
+
+//Set Values Once__________________________________________________________________________________
+$.each(u, function(i) {
+    $('#'+ids.nameID+i).html(u[i].pluralName);
+    u[i].tick = u[i].basetick * u[i].owned;
+});
+$('#'+ids.addNumberID).html(an + ' number');
+//_________________________________________________________________________________________________
+refreshValues();
