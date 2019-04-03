29195cd756a5b0f0c32185d7ba79245dacc6d083
Frank <frankpettersson1@gmail.com>
2019-04-03 10:42:53 +0200

    dark_mode finishing up and css

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,3 @@
-window.outerWidth = window.innerWidth;
 //Hide & Show Function_________________________________________________________
 //Stores state of arrow direction/hidden or shown
 let arrows = new Map();
@@ -27,17 +26,41 @@ for (let i = 0; i <= 7; i++) {
 //_____________________________________________________________________________
 
 //Dark Mode____________________________________________________________________
+function colorLinks(hex)
+{
+    var links = document.getElementsByTagName("a");
+    for(var i=0;i<links.length;i++)
+    {
+        if(links[i].href)
+        {
+            links[i].style.color = hex;  
+        }
+    }  
+}
 let state = 0;
 document.getElementById('darkMode').addEventListener('click', function darkMode() {
+    let width = parseInt(document.getElementById('background').width, 10);
     if (state < 3) {
-        document.getElementById('wrap').style.background = 'rgb(40, 40, 40)';
-        document.getElementById('bg').style.background = 'rgb(20, 20, 20)';
-        document.getElementById('background').src = 'projects/graphics/js_canvas/faded_bubbles_dark/index.html';
+        if (width <= 600) {
+            document.getElementById('wrapwrap').style.background = 'rgb(30, 30, 30)';
+        } else {
+            document.getElementById('background').src = 'projects/graphics/js_canvas/faded_bubbles_dark/index.html';
+        }
+        document.getElementById('wrap').style.background = 'rgb(60, 60, 60)';
+        document.getElementById('titleImage').style.border = '1px dashed white';
+        document.body.style.color = 'white';
+        colorLinks("#FFFFFF");
         state++;
     } else if (state >= 3) {
+        if (width <= 600) {
+            document.getElementById('wrapwrap').style.background = 'rgb(255, 255, 255)';
+        } else {
+            document.getElementById('background').src = 'projects/graphics/js_canvas/faded_bubbles/index.html';
+        }
         document.getElementById('wrap').style.background = 'rgb(255, 255, 255)';
-        document.getElementById('bg').style.background = 'rgb(255, 255, 255)';
-        document.getElementById('background').src = 'projects/graphics/js_canvas/faded_bubbles/index.html';
+        document.getElementById('titleImage').style.border = '1px dashed black';
+        document.body.style.color = 'black';
+        colorLinks("#000000");
         state = 0;
     }
 });
