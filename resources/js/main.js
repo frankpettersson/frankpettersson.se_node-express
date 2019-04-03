f90252bf72ce600d3cdd1dc94795758c0f9ca1dc
frankpettersson <frank.pettersson1@gmail.com>
2019-04-03 21:39:30 +0200

    transparency and dark mode

--- resources/js/main.js
+++ resources/js/main.js
@@ -45,23 +45,35 @@ document.getElementById('darkMode').addEventListener('click', function darkMode(
             document.getElementById('wrapwrap').style.background = 'rgb(30, 30, 30)';
         } else {
             document.getElementById('background').src = 'projects/graphics/js_canvas/faded_bubbles_dark/index.php';
+            document.getElementById('titleImage').style.background = 'rgba(60, 60, 60, 0.5)';
+            document.querySelectorAll('div.box')[0].style.background = 'rgba(50, 50, 50, 0.8)';
+            document.querySelectorAll('div.box')[1].style.background = 'rgba(50, 50, 50, 0.8)';
+            document.querySelectorAll('div.box')[2].style.background = 'rgba(50, 50, 50, 0.8)';
+            document.querySelectorAll('div.box')[3].style.background = 'rgba(50, 50, 50, 0.8)';
         }
-        document.getElementById('wrap').style.background = 'rgb(60, 60, 60)';
-        document.getElementById('titleImage').style.border = '1px dashed white';
+        document.getElementById('wrap').style.background = 'rgba(60, 60, 60, 0.5)';
+        document.getElementById('titleImage').style.border = '2px dashed white';
         document.body.style.color = 'white';
         colorLinks("#FFFFFF");
         state++;
     } else if (state >= 3) {
         if (width <= 600) {
-            document.getElementById('wrapwrap').style.background = 'rgb(255, 255, 255)';
+            document.getElementById('wrapwrap').style.background = 'rgb(205, 205, 205)';
         } else {
             document.getElementById('background').src = 'projects/graphics/js_canvas/faded_bubbles/index.php';
         }
-        document.getElementById('wrap').style.background = 'rgb(255, 255, 255)';
-        document.getElementById('titleImage').style.border = '1px dashed black';
+        document.getElementById('wrap').style.background = 'rgba(200, 200, 200, 0.5)';
+        document.getElementById('titleImage').style.background = 'rgba(215, 215, 215, 0.6)'
+        document.querySelectorAll('div.box')[0].style.background = 'rgba(235, 235, 235, 0.8)';
+        document.querySelectorAll('div.box')[1].style.background = 'rgba(235, 235, 235, 0.8)';
+        document.querySelectorAll('div.box')[2].style.background = 'rgba(235, 235, 235, 0.8)';
+        document.querySelectorAll('div.box')[3].style.background = 'rgba(235, 235, 235, 0.8)';
+        document.getElementById('titleImage').style.border = '2px dashed black';
         document.body.style.color = 'black';
         colorLinks("#000000");
         state = 0;
     }
+    document.getElementById('titleImage').style.backgroundImage = 'url("resources/images/icon/fp_green_512x512.png")';
+    document.getElementById('titleImage').style.backgroundSize = '100% 100%';
 });
 //_____________________________________________________________________________
