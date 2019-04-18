2129fd43cac56f26d684be8f59942a569351cf3d
frankpettersson <frank.pettersson1@gmail.com>
2019-04-18 23:12:55 +0200

    blue

--- header.php
+++ header.php
@@ -1,14 +1,24 @@
-<link rel="apple-touch-icon" sizes="180x180" href="/assets/images/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/icons/android-chrome-192x192.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/icons/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/assets/images/icons/safari-pinned-tab.svg" color="#1bc600">
-    <link rel="shortcut icon" href="/assets/images/icons/favicon.ico">
+<link rel="shortcut icon" href="/assets/images/icons/favicon.ico">
+	<link rel="icon" sizes="16x16 32x32 64x64" href="/assets/images/icons/favicon.ico">
+	<link rel="icon" type="image/png" sizes="196x196" href="/assets/images/icons/favicon-192.png">
+	<link rel="icon" type="image/png" sizes="160x160" href="/assets/images/icons/favicon-160.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/assets/images/icons/favicon-96.png">
+	<link rel="icon" type="image/png" sizes="64x64" href="/assets/images/icons/favicon-64.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/images/icons/favicon-32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/images/icons/favicon-16.png">
+	<link rel="apple-touch-icon" href="/assets/images/icons/favicon-57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/assets/images/icons/favicon-114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/assets/images/icons/favicon-72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/assets/images/icons/favicon-144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/assets/images/icons/favicon-60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/assets/images/icons/favicon-120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/assets/images/icons/favicon-76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/assets/images/icons/favicon-152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/assets/images/icons/favicon-180.png">
+	<meta name="msapplication-TileColor" content="#FFFFFF">
+	<meta name="msapplication-TileImage" content="/favicon-144.png">
+	<meta name="msapplication-config" content="/browserconfig.xml">
     <meta name="apple-mobile-web-app-title" content="frankpettersson.se">
-    <meta name="application-name" content="frankpettersson.se">
-    <meta name="msapplication-TileColor" content="#00a300">
-    <meta name="msapplication-config" content="/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
