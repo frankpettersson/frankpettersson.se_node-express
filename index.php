206361df9b88015677bc3245adf194daf348d985
Frank <frankpettersson1@gmail.com>
2019-04-18 12:14:50 +0200

    descriptions

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <title>frankpettersson.se</title>
 </head>
 <body width="100%" height="100%">
-    <main>
-        <div id="wrap"> 
+    <div id="wrap"> 
+        <header>
             <div class="box nav">
                 <div id="navLeft">
                     <div id="logo">
@@ -21,6 +21,8 @@
                     <span class="link"><a data-show="#projects"">PROJECTS</a></span>
                 </div>
             </div>
+        </header>
+        <main>
             <div id="canvas" class="box box-iframe">
                 <h1 id="canvas-title">BUBBLE BG DARK</h1>
                 <iframe title="canvasiframe" id="frontframe" src="projects/graphics/js_canvas/faded_bubbles_dark/" frameborder="0" scrolling="no"></iframe>
@@ -50,44 +52,154 @@
                     </div>
                 </div>
             </div>
-            <div class="box" id="projects">
+            <div id="projects" class="box">
                 <div class="boxHeader"><h3>Projects</h3></div> 
                 <div id="games" class="category">
                     <img src="assets/images/util/triangle.svg" class="arrow" alt="hide/show">
                     <div class="categoryName">Games</div>
-                    <div class="grid">
-                        <div class="project"><a class="thumbnail" href="projects/games/idle/idle_0.4_no_jquery/"><img src="assets/images/thumbnails/idle.webp" alt="idle game plain javascript"></a><p>Idle game</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/games/idle/idle_0.3_jquery/"><img src="assets/images/thumbnails/idle.webp" alt="idle game jquery"></a><p>Idle game, jQuery</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/games/units_create_units/"><img src="assets/images/thumbnails/units_creating_units.webp" alt="units creating units"></a><p>Units</p></div>
+                    <div class="project-list">
+                        <div class="project"><a class="thumbnail" href="projects/games/idle/idle_0.4_no_jquery/"><img src="assets/images/thumbnails/idle.webp" alt="idle game plain javascript"></a>
+                            <p>
+                                <b>Idle game</b> - 2019-02-25<br>
+                                My first "real" game. You click on a button to get gold and then with that gold purchase units to collect even more gold for you.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/games/idle/idle_0.3_jquery/"><img src="assets/images/thumbnails/idle.webp" alt="idle game jquery"></a>
+                            <p>
+                                <b>Idle game, jQuery</b> - 2019-02-21<br>
+                                Same as the last one, just written with a code library.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/games/units_create_units/"><img src="assets/images/thumbnails/units_creating_units.webp" alt="units creating units"></a>
+                            <p>
+                                <b>Units</b> - 2019-04-02<br>
+                                Another attempt at a game. I wanted to try making a game where the units create eachother, so I made this where that is so and you can name/create your own units too.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div id="graphics" class="category">
                     <img src="assets/images/util/triangle.svg" class="arrow" alt="hide/show">
                     <div class="categoryName">Graphics</div>
-                    <div class="grid">
-                        <div class="project"><a class="thumbnail" href="projects/graphics/pixi_js/rotatingRectangle/"><img src="assets/images/thumbnails/rotating_rectangle.webp" alt="rotating rectangle"></a><p>Rectangle</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/pixi_js/rotatingPolygons/"><img src="assets/images/thumbnails/rotating_polygon.webp" alt="rotating polygon"></a><p>Polygon</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/pixi_js/randomPolygonPositions/"><img src="assets/images/thumbnails/random_polygons.webp" alt="random polygons"></a><p>Random polygon</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/bubbles/"><img src="assets/images/thumbnails/bubbles_paper.webp" alt="bubbles paper.js"></a><p>Bubbles, 400x400</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/pixelGrid_blurry/"><img src="assets/images/thumbnails/pixels_blurry.webp" alt="blurry pixels"></a><p>Blurry pixels</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/pixelGrid_clear/"><img src="assets/images/thumbnails/pixels_clear.webp" alt="clear pixels"></a><p>Clear pixels</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/langtonsAttempt/"><img src="assets/images/thumbnails/langton_attempt.webp" alt="langtons ant attempt"></a><p>Langtons failure</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/langtonsAnt/"><img src="assets/images/thumbnails/langtons_ant.webp" alt="langtons ant"></a><p>Langtons ant</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/three.js/3Dspheres/"><img src="assets/images/thumbnails/spheres.webp" alt="3d spheres with camera controls"></a><p>3D Shapes</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/three.js/3Dspheres_BG/"><img src="assets/images/thumbnails/sphere_BG.webp" alt="3d spheres static camera"></a><p>3D Shapes BG</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/colours/"><img src="assets/images/thumbnails/colours.webp" alt="colour canvas"></a><p>Colours spinning</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/black_hole/"><img src="assets/images/thumbnails/black_hole.webp" alt="black hole"></a><p>Black hole</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/bubbles/"><img src="assets/images/thumbnails/bubbles.webp" alt="bubbles"></a><p>Bubbles</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/faded_bubbles/"><img src="assets/images/thumbnails/faded_bubbles.webp" alt="faded bubbles"></a><p>Faded bubbles</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/black_donut/"><img src="assets/images/thumbnails/black_donut.webp" alt="black donut"></a><p>Black donut</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/look_in_the_middle/"><img src="assets/images/thumbnails/look.webp" alt="look in the middle illusion"></a><p>Point illusion</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/three.js/hollow_boxes/"><img src="assets/images/thumbnails/hollow.webp" alt="hollow boxes"></a><p>Hollow boxes</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/flowing_squares/"><img src="assets/images/thumbnails/squares.webp" alt="flowing squares"></a><p>Flowing squares</p></div>
-                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/better_ant/"><img src="assets/images/thumbnails/bant.webp" alt="better ant"></a><p>Better ant</p></div>
+                    <div class="project-list">
+                        <div class="project"><a class="thumbnail" href="projects/graphics/pixi_js/rotatingRectangle/"><img src="assets/images/thumbnails/rotating_rectangle.webp" alt="rotating rectangle"></a>
+                            <p>
+                                <b>Rectangle</b> - 2019-02-26<br>
+                                First canvas drawing in JavaScript i managed to create, using a library: <a href="http://www.pixijs.com/" target="_blank">PixiJS</a>, that I used for the next 3 aswell.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/pixi_js/rotatingPolygons/"><img src="assets/images/thumbnails/rotating_polygon.webp" alt="rotating polygon"></a>
+                            <p>
+                                <b>Polygon</b> - 2019-02-28<br>
+                                Created a polygon, bit harded since you have to defined the points of the shape, also made it spin :).
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/pixi_js/randomPolygonPositions/"><img src="assets/images/thumbnails/random_polygons.webp" alt="random polygons"></a>
+                            <p>
+                                <b>Random polygon</b> - 2019-02-28<br>
+                                Went a bit further with the polygon creation. In this one the points defining the shape are moving around randomly(a bit jittery, but funny).
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/bubbles/"><img src="assets/images/thumbnails/bubbles_paper.webp" alt="bubbles paper.js"></a>
+                            <p>
+                                <b>Bubbles paper.js</b> - 2019-03-01<br>
+                                Did not actually make this one myself although i thought it looked cool, and so later I made one myself without the help of PixiJS.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/pixelGrid_blurry/"><img src="assets/images/thumbnails/pixels_blurry.webp" alt="blurry pixels"></a>
+                            <p>
+                                <b>Blurry pixels</b> - 2019-03-01<br>
+                                Trying another library: <a href="http://paperjs.org/" target="_blank">paper.js</a> which I use for the next 3 projects. Played around with squares, pixel-grids and colours, the blurry effect is actually me not scaling the squares the right amount, albeit the bug, cool effect.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/pixelGrid_clear/"><img src="assets/images/thumbnails/pixels_clear.webp" alt="clear pixels"></a>
+                            <p>
+                                <b>Clear pixels</b> - 2019-03-01<br>
+                                "Fixed" the last one and got the right resolution for the squares making it much clearer.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/langtonsAttempt/"><img src="assets/images/thumbnails/langton_attempt.webp" alt="langtons ant attempt"></a>
+                            <p>
+                                <b>Langtons failure</b> - 2019-03-02<br>
+                                I've known about <a href="https://en.wikipedia.org/wiki/Langton%27s_ant" target="_blank">Langtons Ant</a> for awhile and wanted to tackle it since i'd learnt a bit about canvas coding, did not work as expected. Looks pretty.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/paper.js/langtonsAnt/"><img src="assets/images/thumbnails/langtons_ant.webp" alt="langtons ant"></a>
+                            <p>
+                                <b>Langtons ant</b> - 2019-03-02<br>
+                                Working version of <a href="https://en.wikipedia.org/wiki/Langton%27s_ant" target="_blank">Langtons Ant</a>.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/three.js/3Dspheres/"><img src="assets/images/thumbnails/spheres.webp" alt="3d spheres with camera controls"></a>
+                            <p>
+                                <b>3D Shapes</b> - 2019-03-06<br>
+                                Found a library: <a href="https://threejs.org/" target="_blank">three.js</a> making it easy to create 3D shapes and animations. So, i defined a cube, plopped a couple of rotating shapes with random colors and added camera controls.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/three.js/3Dspheres_BG/"><img src="assets/images/thumbnails/sphere_BG.webp" alt="3d spheres static camera"></a>
+                            <p>
+                                <b>3D Shapes BG</b> - 2019-03-07<br>
+                                Edited the last one a bit so that it could be used as a background, the moving is actually the camera rotating along its axes.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/colours/"><img src="assets/images/thumbnails/colours.webp" alt="colour canvas"></a>
+                            <p>
+                                <b>Colours spinning</b> - 2019-03-20<br>
+                                Having a bit more experience I decided to move away from using libraries to learn more about the basics of JavaScript. Which turned out to be a good idea making me discover a technique for creating cool patterns. Such as this one.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/black_hole/"><img src="assets/images/thumbnails/black_hole.webp" alt="black hole"></a>
+                            <p>
+                                <b>Black hole</b> - 2019-03-20<br>
+                                This one is one of my favourites, just because it feels very clean and reminds me of being little and seeing similar shapes and animations which would get me stuck staring at them for just a bit too long.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/bubbles/"><img src="assets/images/thumbnails/bubbles.webp" alt="bubbles"></a>
+                            <p>
+                                <b>Bubbles</b> - 2019-03-21<br>
+                                Recreated the canvas from earlier but without any library although lacking the depth effect.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/faded_bubbles/"><img src="assets/images/thumbnails/faded_bubbles.webp" alt="faded bubbles"></a>
+                            <p>
+                                <b>Faded bubbles</b> - 2019-03-22<br>
+                                Successful recreated version with depth effect and even a splash of color.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/black_donut/"><img src="assets/images/thumbnails/black_donut.webp" alt="black donut"></a>
+                            <p>
+                                <b>Black donut</b> - 2019-03-23<br>
+                                This one I really like, not much to say but, it's cool.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/look_in_the_middle/"><img src="assets/images/thumbnails/look.webp" alt="look in the middle illusion"></a>
+                            <p>
+                                <b>Point illusion</b> - 2019-03-23<br>
+                                Actually the same one as the one last, difference being is the circle motioned drawing speed. Making for a very slowly adding of blackness with expanding patterns that you barely notice at first.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/three.js/hollow_boxes/"><img src="assets/images/thumbnails/hollow.webp" alt="hollow boxes"></a>
+                            <p>
+                                <b>Hollow boxes</b> - 2019-04-09<br>
+                                For this one I went back to the 3D shape/webGL library <a href="https://threejs.org/" target="_blank">three.js</a> and played around with stacking boxes further away but with decreasing size, using a depth material for the mesh, and blabla. Looks nice.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/flowing_squares/"><img src="assets/images/thumbnails/squares.webp" alt="flowing squares"></a>
+                            <p>
+                                <b>Flowing squares</b>  - 2019-04-12<br>
+                                This one uses various techniques that i've learned such as border detection, Class creation, moving a defined shape etc. creating this rug of squares, bouncing from wall to wall. Like those DVD screensavers.
+                            </p>
+                        </div>
+                        <div class="project"><a class="thumbnail" href="projects/graphics/js_canvas/better_ant/"><img src="assets/images/thumbnails/bant.webp" alt="better ant"></a>
+                            <p>
+                                <b>Better ant</b> - 2019-04-11<br>
+                                Browsing the web i found <a href="http://rossscrivener.co.uk/projects/langtons-ants/" target="_blank">this</a> demonstration of Langtons Ant(which btw is alot cooler than mine), and noticed the much better performance, so being inspired to improve my attempt I did so. And also changed the color.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
+        </main>
+    </div>
 </body>
 </html>
