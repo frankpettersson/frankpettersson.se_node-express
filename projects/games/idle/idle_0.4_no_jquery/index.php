46cf7e40f1ec6c6a9f4e0bfbb4b12ae3b31029f2
Frank <frankpettersson1@gmail.com>
2019-05-10 14:32:28 +0200

    pug

--- projects/games/idle/idle_0.4_no_jquery/index.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php include "../../../../header.php" ?>
-    <link rel="stylesheet" href="../resources/style/idle.css">
-    <script async src="js/main.js"></script>
-    <title>Idle game, plain JavaScript</title>
-</head><body></body></html>
