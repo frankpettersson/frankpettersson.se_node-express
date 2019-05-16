2e84d4c3fd850b6ba901532e1703a36d2ba56698
Frank <frankpettersson1@gmail.com>
2019-05-16 17:10:58 +0200

    settings and other

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,36 +1,99 @@
-//GET UI COLOR
-let UI_COLOR = localStorage.getItem('UI-COLOR');
-if (UI_COLOR !== null) {
-    updateUI(UI_COLOR);
-} else {
-    updateUI('#ff009d');
-}
-
-//SET UI COLOR
+//DEFAULT SETTINGS
 let activeLink = 0;
+let defaultSettingText = 'Stored: ';
+
+//GET SETTINGS
+let DEFAULT_PROJECT = localStorage.getItem('DEFAULT_PROJECT');
+let DEFAULT_PROJECT_LABEL = localStorage.getItem('DEFAULT_PROJECT_LABEL');
+let DEFAULT_PROJECT_IMAGE = localStorage.getItem('DEFAULT_PROJECT_IMAGE');
+let LANDING_PAGE = localStorage.getItem('LANDING_PAGE');
+let UI_COLOR = localStorage.getItem('UI_COLOR');
+
+//DEFAULT PROJECT
+DEFAULT_PROJECT ? updateDefaultCanvas(DEFAULT_PROJECT, DEFAULT_PROJECT_LABEL, DEFAULT_PROJECT_IMAGE) : updateDefaultCanvas('projects/graphics/js_canvas/flowing_squares/index.html', 'FLOWING SQUARES', 'assets/images/thumbnails/flowing squares.png');
+
+//LANDING PAGE
+LANDING_PAGE ? updateLandingPage(LANDING_PAGE) : updateLandingPage('PROJECTS');
+
+//UI COLOR
+UI_COLOR ? updateUI(UI_COLOR) : updateUI('#ff009d');
+
+//SAVE SETTINGS
 document.querySelector('#save').addEventListener('click', () => {
+    //SET UI COLOR
     let newUIColor = document.querySelector('#select-ui-color').value;
     document.querySelectorAll('.link a')[activeLink].style.color = newUIColor;
-    document.querySelector('#current-ui-color').innerText = newUIColor;
-    document.querySelector('#current-ui-color').style.color = newUIColor;
     updateUI(newUIColor);
+    //SET LANDING PAGE
+    let newLandingPage = document.querySelector('#select-landing-page').value;
+    document.querySelector('#current-landing-page').innerText = defaultSettingText+newLandingPage;
+    document.querySelector('#select-landing-page').value = newLandingPage;
+    localStorage.setItem('LANDING_PAGE', newLandingPage);
+    //SET DEFAULT PROJECT
+    let newDefaultCanvas = document.querySelector('#select-default-project').value;
+    let newDefaultCanvasLabel = document.querySelector('#select-default-project').options[document.querySelector('#select-default-project').selectedIndex].label;
+    let newDefaultCanvasImage = 'assets/images/thumbnails/'+document.querySelector('#select-default-project').options[document.querySelector('#select-default-project').selectedIndex].label.toLowerCase()+'.png';
+    updateDefaultCanvas(newDefaultCanvas, newDefaultCanvasLabel, newDefaultCanvasImage);
 });
 
+//RESET SETTINGS
 document.querySelector('#reset').addEventListener('click', () => {
-    document.querySelectorAll('.link a')[activeLink].style.color ='#ff009d';
-    document.querySelector('#current-ui-color').innerText = '#ff009d';
-    document.querySelector('#current-ui-color').style.color = '#ff009d';
+    //RESET UI COLOR
+    document.querySelectorAll('.link a')[activeLink].style.color = '#ff009d';
     updateUI('#ff009d');
+    //RESET LANDING PAGE
+    document.querySelector('#current-landing-page').innerText = defaultSettingText+'PROJECTS';
+    document.querySelector('#select-landing-page').value = 'PROJECTS';
+    localStorage.setItem('LANDING_PAGE', 'PROJECTS');
+    //RESET DEFAULT PROJECT
+    updateDefaultCanvas('projects/graphics/js_canvas/flowing_squares/index.html', 'FLOWING SQUARES', 'assets/images/thumbnails/flowing squares.png');
 });
+
+//UPDATE DEFAULT PROJECT
+function updateDefaultCanvas(project, label, image) {
+    document.getElementById('frontframe').src = project;
+    document.getElementById('project-title').innerText = label;
+    document.querySelector('#current-default-project').innerText = defaultSettingText+label;
+    document.querySelector('#current-default-project-image').src = image;
+    document.querySelector('#current-default-project-image').alt = 'default '+label;
+    document.querySelector('#select-default-project').value = project;
+    localStorage.setItem('DEFAULT_PROJECT', project);
+    localStorage.setItem('DEFAULT_PROJECT_LABEL', label);
+    localStorage.setItem('DEFAULT_PROJECT_IMAGE', image);
+}
+
+//UPDATE LANDING PAGE
+function updateLandingPage(page) {
+    document.querySelector('#current-landing-page').innerText = defaultSettingText+page;
+    if (page === 'PROJECTS') {
+        document.querySelector('#select-landing-page').value = page;
+        document.querySelectorAll('.link a')[0].style.color = UI_COLOR;
+        document.querySelectorAll('.mobile-link a')[0].style.color = UI_COLOR;
+        document.querySelector('#projects').style.display = 'block';
+        document.querySelector('#descriptions').style.display = 'none';
+        document.querySelector('#settings').style.display = 'none';
+    } else if (page === 'DESCRIPTIONS') {
+        document.querySelector('#select-landing-page').value = page;
+        document.querySelectorAll('.link a')[1].style.color = UI_COLOR;
+        document.querySelectorAll('.mobile-link a')[1].style.color = UI_COLOR;
+        document.querySelector('#projects').style.display = 'none';
+        document.querySelector('#descriptions').style.display = 'block';
+        document.querySelector('#settings').style.display = 'none';
+    } else if (page === 'SETTINGS') {
+        document.querySelector('#select-landing-page').value = page;
+        document.querySelectorAll('.link a')[2].style.color = UI_COLOR;
+        document.querySelectorAll('.mobile-link a')[2].style.color = UI_COLOR;
+        document.querySelector('#projects').style.display = 'none';
+        document.querySelector('#descriptions').style.display = 'none';
+        document.querySelector('#settings').style.display = 'block';
+    }
+}
 
 //UPDATE UI COLOR
-document.querySelectorAll('.link a')[activeLink].style.color = UI_COLOR;
-document.querySelectorAll('.link-mobile a')[activeLink].style.color = UI_COLOR;
-document.querySelector('#current-ui-color').innerText = UI_COLOR;
-document.querySelector('#current-ui-color').style.color = UI_COLOR;
-
 function updateUI(color) {
-    localStorage.setItem('UI-COLOR', color);
+    localStorage.setItem('UI_COLOR', color);
+    document.querySelector('#current-ui-color').innerText = defaultSettingText+color;
+    document.querySelector('#current-ui-color').style.color = color;
     document.querySelector('#select-ui-color').value = color;
 
     //LOGO COLOR CHANGE
@@ -67,59 +130,63 @@ function updateUI(color) {
     let mobileTouchEnd = e => e.addEventListener('touchend', e => e.target.style.color = 'rgb(30, 30, 30)', {passive: true});
     [...document.querySelectorAll('.project')].forEach(mobileTouchEnd);
 
+    //NAVBAR PAGE SELECTION
     //NAVBAR LINK COLOR CHANGES
     let linkClick = e => e.addEventListener('click', e => {
         let target = e.target.getAttribute('data-show');
+        document.querySelector('#menu-btn').checked = false;
         if (target == 0) {
             document.querySelectorAll('.link a')[0].style.color = color;
             document.querySelectorAll('.link a')[1].style.color = 'rgb(30, 30, 30)';
             document.querySelectorAll('.link a')[2].style.color = 'rgb(30, 30, 30)';
-            document.querySelectorAll('.link-mobile a')[0].style.color = color;
-            document.querySelectorAll('.link-mobile a')[1].style.color = 'rgb(30, 30, 30)';
-            document.querySelectorAll('.link-mobile a')[2].style.color = 'rgb(30, 30, 30)';
-            document.querySelector('#projects').style.display = 'none';
+            document.querySelectorAll('.mobile-link a')[0].style.color = color;
+            document.querySelectorAll('.mobile-link a')[1].style.color = 'rgb(30, 30, 30)';
+            document.querySelectorAll('.mobile-link a')[2].style.color = 'rgb(30, 30, 30)';
+            document.querySelector('#projects').style.display = 'block';
+            document.querySelector('#descriptions').style.color = 'rgb(30, 30, 30)';
+            document.querySelector('#descriptions').style.display = 'none';
+            document.querySelector('#settings').style.color = 'rgb(30, 30, 30)';
             document.querySelector('#settings').style.display = 'none';
-            document.querySelector('#canvas').style.display = 'block';
-            document.querySelector('#canvi').style.display = 'block';
+            
             activeLink = 0;
         } else if (target == 1) {
             document.querySelectorAll('.link a')[0].style.color = 'rgb(30, 30, 30)';
             document.querySelectorAll('.link a')[1].style.color = color;
             document.querySelectorAll('.link a')[2].style.color = 'rgb(30, 30, 30)';
-            document.querySelectorAll('.link-mobile a')[0].style.color = 'rgb(30, 30, 30)';
-            document.querySelectorAll('.link-mobile a')[1].style.color = color;
-            document.querySelectorAll('.link-mobile a')[2].style.color = 'rgb(30, 30, 30)';
-            document.querySelector('#canvas').style.display = 'none';
-            document.querySelector('#canvas').style.display = 'none';
-            document.querySelector('#canvi').style.display = 'none';
+            document.querySelectorAll('.mobile-link a')[0].style.color = 'rgb(30, 30, 30)';
+            document.querySelectorAll('.mobile-link a')[1].style.color = color;
+            document.querySelectorAll('.mobile-link a')[2].style.color = 'rgb(30, 30, 30)';
+            document.querySelector('#project-iframe').style.color = 'rgb(30, 30, 30)';
+            document.querySelector('#projects').style.color = 'rgb(30, 30, 30)';
+            document.querySelector('#projects').style.display = 'none';
+            document.querySelector('#descriptions').style.display = 'block';
+            document.querySelector('#settings').style.color = 'rgb(30, 30, 30)';
             document.querySelector('#settings').style.display = 'none';
-            document.querySelector('#projects').style.display = 'block';
             activeLink = 1;
         } else if (target == 2) {
             document.querySelectorAll('.link a')[0].style.color = 'rgb(30, 30, 30)';
             document.querySelectorAll('.link a')[1].style.color = 'rgb(30, 30, 30)';
             document.querySelectorAll('.link a')[2].style.color = color;
-            document.querySelectorAll('.link-mobile a')[0].style.color = 'rgb(30, 30, 30)';
-            document.querySelectorAll('.link-mobile a')[1].style.color = 'rgb(30, 30, 30)';
-            document.querySelectorAll('.link-mobile a')[2].style.color = color;
-            document.querySelector('#canvas').style.display = 'none';
-            document.querySelector('#canvas').style.color = 'rgb(30, 30, 30)';
-            document.querySelector('#canvi').style.display = 'none';
-            document.querySelector('#projects').style.display = 'none';
+            document.querySelectorAll('.mobile-link a')[0].style.color = 'rgb(30, 30, 30)';
+            document.querySelectorAll('.mobile-link a')[1].style.color = 'rgb(30, 30, 30)';
+            document.querySelectorAll('.mobile-link a')[2].style.color = color;
+            document.querySelector('#project-iframe').style.color = 'rgb(30, 30, 30)';
             document.querySelector('#projects').style.color = 'rgb(30, 30, 30)';
+            document.querySelector('#projects').style.display = 'none';
+            document.querySelector('#descriptions').style.display = 'none';
             document.querySelector('#settings').style.display = 'block';
             activeLink = 2;
         }
     });
     [...document.querySelectorAll('.link a')].forEach(linkClick);
-    [...document.querySelectorAll('.link-mobile a')].forEach(linkClick);
+    [...document.querySelectorAll('.mobile-link a')].forEach(linkClick);
 
     let linkOver = e => e.addEventListener('mouseover', e => {if (e.target.getAttribute('data-show') != activeLink) e.target.style.color = color});
     [...document.querySelectorAll('.link a')].forEach(linkOver);
-    [...document.querySelectorAll('.link-mobile a')].forEach(linkOver);
+    [...document.querySelectorAll('.mobile-link a')].forEach(linkOver);
     let linkOut = e => e.addEventListener('mouseout', e => {if (e.target.getAttribute('data-show') != activeLink) e.target.style.color = 'rgb(30, 30, 30)'});
     [...document.querySelectorAll('.link a')].forEach(linkOut);
-    [...document.querySelectorAll('.link-mobile a')].forEach(linkOut);
+    [...document.querySelectorAll('.mobile-link a')].forEach(linkOut);
 }
 
 //HIDE/SHOW PROJECT CATEGORY ARROW
@@ -129,13 +196,13 @@ let arrowClick = e => e.addEventListener('click', e => {
 });
 [...document.querySelectorAll('.arrow')].forEach(arrowClick);
 
-//PREVIEW CANVAS CHANGER
-let canvasClick = e => e.addEventListener('click', e => {
+//PREVIEW PROJECT CHANGER
+let projectClick = e => e.addEventListener('click', e => {
     if (e.target.nodeName.toLowerCase() === "img") {
         let src = e.target.getAttribute('data-src');
         document.getElementById('frontframe').src = src;
-        document.getElementById('canvas-title').innerText = e.target.parentElement.nextElementSibling.innerText;
+        document.getElementById('project-title').innerText = e.target.parentElement.nextElementSibling.innerText;
         window.scrollTo(0, 0);
     }
 });
-[...document.querySelectorAll('#canvi .category .grid .project .thumbnail')].forEach(canvasClick);
+[...document.querySelectorAll('#projects .category .grid .project .thumbnail')].forEach(projectClick);
